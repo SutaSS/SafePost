494502b6748e422bbffac90cfddd55a0cbbc6b66
Andikasaktih <andikasaktih@gmail.com>
2026-03-04 12:55:26 +0700

    fix: Seo Generateor

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,3 +1,5 @@
+@use('Artesaos\SEOTools\Facades\SEOTools')
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    {!! SEO::generate() !!}
+    {!! SEOTools::generate() !!}
 
     <title>@yield('title', 'SafePost')</title>
 
